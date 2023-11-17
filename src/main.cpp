ba2594c2f7f2e01dc6743a98622ae6ab1e9077e6
mohamedibrahim155 <63718196+mohamedibrahim155@users.noreply.github.com>
2023-11-17 16:35:41 -0500

    Protocol buffer created

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-#include "../Shared/addressbook.pb.h"
+#include <fstream>
+#include <string>
+#include "../Shared/authentication.pb.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-	//tutorial::AddressBook temp;
+	CreateAccountWeb web;
+	web.set_request_id(1);
+	web.set_email("Mohamed@gmail.com");
+	web.set_plaintext_password("MypassWord");
+
+	std::string parsedData;
+	web.SerializeToString(&parsedData);
+	
+    
+	std::cout << "Paresed Data : " << parsedData << std::endl;
+	
+	CreateAccountWeb webReq;
+
+	bool result =webReq.ParseFromString(parsedData);
+	if (result)
+	{
+		std::cout << webReq.request_id() << std::endl;
+		std::cout << webReq.email()<<std::endl;
+		std::cout << webReq.plaintext_password() << std::endl;
+	}
 
 	return 0;
 }
